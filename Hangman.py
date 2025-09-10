40da7865365bdd3b05efd7bc5610bc1ebe67f47e
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-10 15:49:51 +0100

    word list format change

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,5 @@
 import random
 
-wordList=[Apple,
-banana,
-melon,
-orange,
-grape,
-pear]
+wordList=[Apple,banana,melon,orange,grape,pear]
 chosenWord=wordList[random.randint(0,6)]
 print(chosenWord)
