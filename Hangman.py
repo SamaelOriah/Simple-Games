50eb11c797ddf76c6b3d6c4c9dd622b08defd1f7
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-10 23:20:18 +0000

    Completed game as of now. Added attempts and fixed the while loop so it works properly.

--- Hangman.py
+++ Hangman.py
@@ -1,18 +1,27 @@
 import random
 
 wordList=["apple","banana","melon","orange","grape","pear"]
-chosenWord=wordList[random.randint(0,6)]
+chosenWord=wordList[random.randint(0,5)]
 letters=[]
-missLet=0
-while missLet==0:
+finWord=False
+attempts=8
+while finWord==False:
+  missLet=0
   for x in chosenWord:
     if x in letters:
       print(x,end=" ")
     else:
       print("_",end=" ")
       missLet+=1
-  if missLet==0:
-    print(f"Well done! The word was {chosenWord}")
 
-  guess=input("Please guess a letter in the word: ")
+  if missLet==0:
+    finWord=True
+    print(f"\nWell done! The word was {chosenWord}")
+  elif attempts==0:
+    print(f"Bad Luck! The word was {chosenWord}.")
+  print()
+  guess=input("\nPlease guess a letter in the word: ")
+  if guess not in chosenWord:
+    attempts-=1
+    print(f"\nNot in word. {attempts} attempts left.")
   letters.append(guess)
