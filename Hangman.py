54301e11b3a6d3b28347135aca642aae245d7755
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-10 23:57:37 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -2,4 +2,17 @@ import random
 
 wordList=["apple","banana","melon","orange","grape","pear"]
 chosenWord=wordList[random.randint(0,6)]
-print(chosenWord)
+letters=[]
+missLet=0
+while missLet==0:
+  for x in chosenWord:
+    if x in letters:
+      print(x,end=" ")
+    else:
+      print("_",end=" ")
+      missLet+=1
+  if missLet==0:
+    print(f"Well done! The word was {chosenWord}")
+
+  guess=input("Please guess a letter in the word: ")
+  letters.append(guess)
