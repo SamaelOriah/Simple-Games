d2bf94b3c2fc68f95fe1e0807deeb35ea24b362f
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-11 16:48:44 +0000

    Added more to the Help Function

--- HigherLower.py
+++ HigherLower.py
@@ -4,6 +4,9 @@ def Help():
     Easy= between 1 and 20
     Medium= between 1 and 40
     Hard = between 1 and 100
+    
+    You will guess a number between that range and you will be told if you need to go lower or higher.
+    Depending on your difficulty you have a certain number of attempts to do it in.
           """)
 print("Welcome to Higher or Lower. Would you like to read the rules?")
 ans=input("Enter your response here >> ").upper
