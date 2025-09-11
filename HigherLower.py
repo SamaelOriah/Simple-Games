a08c591abeca50669d4707acdcde17cb8fcb17f7
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-11 16:45:52 +0000

    Created game. Started help page.

--- HigherLower.py
+++ HigherLower.py
@@ -1,1 +1,11 @@
-
+def Help():
+    print("""A random number is chosen depending on your preferred difficulty level.
+        
+    Easy= between 1 and 20
+    Medium= between 1 and 40
+    Hard = between 1 and 100
+          """)
+print("Welcome to Higher or Lower. Would you like to read the rules?")
+ans=input("Enter your response here >> ").upper
+if ans=="Y" or "YES":
+    Help()
